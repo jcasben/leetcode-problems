42a56a13235ceccb66fe5a645a0e59112d9e6472
jcasben <jcasben02@gmail.com>
2023-09-02 13:40:31 +0200

    [UPDATE: TwoSum.java] Re factorized the code.

--- 2. Two Sum/TwoSum.java
+++ 2. Two Sum/TwoSum.java
@@ -1,18 +1,16 @@
 public class TwoSum {
-  public static void main(String[] args) {
-    int [] nums = {3,2,4};
-    int target = 6;
-
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = 0; j < nums.length; j++) {
-        if(j == i) {
-          continue;
-        } else if(nums[i] + nums[j] == target) {
-          int [] sol = {i, j};
-          System.out.println(i + ", " + j);
-          return;
+    public static void main(String[] args) {
+        int [] nums = {3,2,4};
+        int target = 6;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue;
+                } else if (nums[i] + nums[j] == target) {
+                    System.out.println(i + ", " + j);
+                    return;
+                }
+            }
         }
-      }
     }
-  }
 }
